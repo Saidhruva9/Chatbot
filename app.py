774ef6c4e69e10c973732c79527993ef5ba9ffe3
Saidhruva9 <saidhruvagade@gmail.com>
2025-04-27 19:24:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def chat():
         return jsonify({"error": str(e)}), 500  # Handle errors
 
 if __name__ == '__main__':
-     port = int(os.environ.get('PORT', 5000))
-     app.run(host='0.0.0.0', port=port, debug=True)
+     port = int(os.environ.get('PORT',5000))
+     app.run(host='0.0.0.0',port=port,debug=True)
