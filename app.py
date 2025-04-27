eb36e634df936446a94bcb0846423ce8b7b0a5a4
Saidhruva9 <saidhruvagade@gmail.com>
2025-04-27 19:18:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -28,4 +29,5 @@ def chat():
         return jsonify({"error": str(e)}), 500  # Handle errors
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
